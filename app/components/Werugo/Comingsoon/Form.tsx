d9c17cc8363411f747b063d023d69eccb0fd43dc
webdev-kamran <146112161+webdev-kamran@users.noreply.github.com>
2024-06-29 00:36:29 +0530

    werugo timer has been added

--- app/components/Werugo/Comingsoon/Form.tsx
+++ app/components/Werugo/Comingsoon/Form.tsx
@@ -7,7 +7,7 @@ const Form = () => {
   const [formData, setFormData] = useState({ name: '', email: '' });
   const [submitted, setSubmitted] = useState(false);
 
-  const handleChange = (e:any) => {
+  const handleChange = (e: any) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
@@ -15,7 +15,7 @@ const Form = () => {
     });
   };
 
-  const handleSubmit = (e:any) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     console.log('Form submitted:', formData);
     setSubmitted(true);
@@ -26,24 +26,24 @@ const Form = () => {
       {submitted ? (
         <div className="flex justify-center items-center">
           <div className='space-y-2'>
-<Image src={Success} alt="success" width={100} height={100} className='mx-auto w-auto h-auto' />
-<p className='text-black font-medium text-center'>Subscribed!</p>
+            <Image src={Success} alt="success" width={100} height={100} className='mx-auto w-auto h-auto' />
+            <p className='text-black font-medium text-center'>Subscribed!</p>
           </div>
         </div>
       ) : (
+      <>
         <form onSubmit={handleSubmit}>
-
-
-<div className='space-y-1'>
+          <fieldset className='space-y-1'>
             <label className='text-left text-black font-medium'>
-            Get notified when launched
+              Get notified when launched
             </label>
             <div className='flex gap-2'>
               <input onChange={handleChange} required type='email' className='bg-black/5 outline-none focus:ring-1 focus:ring-[#39AE56] rounded-xl py-3 px-5 placeholder-black/50 flex-1' placeholder='Email address' />
-            <button type="submit" className="rounded-xl font-semibold text-white leading-snug flex gap-2 py-3 px-6 items-center justify-center  ease-in-out duration-300  bg-[#39AE56] text-lg shrink-0">Subscribe</button>
+              <button type="submit" className="rounded-xl font-semibold text-white leading-snug flex gap-2 py-3 px-6 items-center justify-center  ease-in-out duration-300  bg-[#39AE56] text-lg shrink-0">Subscribe</button>
             </div>
-          </div> 
+          </fieldset>
         </form>
+        </>
       )}
     </div>
   );
